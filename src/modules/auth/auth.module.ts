201ccf7ff0c2a13b2edd22065d30fe287496c7b5
Adriano hilario ferreira <adrianoferreira31@gmail.com>
2023-11-15 10:18:18 -0300

    :construction: creating validations with auth guard

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,17 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { JwtModule } from '@nestjs/jwt';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 import { env } from 'src/shared/config/env';
 
 @Module({
   imports: [
     JwtModule.register({
+      global: true,
       signOptions: { expiresIn: '7d' },
       secret: env.jwtSecret,
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtService],
 })
 export class AuthModule {}
