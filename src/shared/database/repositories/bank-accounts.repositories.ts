5f69987c536916ece8ee0ff558fdeb0f79877220
Adriano hilario ferreira <adrianoferreira31@gmail.com>
2024-01-02 17:30:04 -0300

    :construction: add method of deleting bank account

--- src/shared/database/repositories/bank-accounts.repositories.ts
+++ src/shared/database/repositories/bank-accounts.repositories.ts
@@ -19,4 +19,8 @@ export class BankAccountsRepository {
   update(updateDto: Prisma.BankAccountUpdateArgs) {
     return this.prismaService.bankAccount.update(updateDto);
   }
+
+  delete(deleteDto: Prisma.BankAccountDeleteArgs) {
+    return this.prismaService.bankAccount.delete(deleteDto);
+  }
 }
