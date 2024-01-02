61afcdfab88946aaf5ee3943bb8963b8db5d900c
Adriano hilario ferreira <adrianoferreira31@gmail.com>
2024-01-02 15:25:11 -0300

    :construction: creation of the bank accounts module

---
+++ src/modules/bank-accounts/bank-accounts.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { CreateBankAccountDto } from './dto/create-bank-account.dto';
+import { UpdateBankAccountDto } from './dto/update-bank-account.dto';
+import { BankAccountsRepository } from 'src/shared/database/repositories/bank-accounts.repositories';
+
+@Injectable()
+export class BankAccountsService {
+  constructor(private readonly bankAccountsRepo: BankAccountsRepository) {}
+  create(userId: string, createBankAccountDto: CreateBankAccountDto) {
+    const { color, initialBalance, name, type } = createBankAccountDto;
+    return this.bankAccountsRepo.create({
+      data: {
+        userId,
+        color,
+        initialBalance,
+        name,
+        type,
+      },
+    });
+  }
+
+  findAllByUserId(userId: string) {
+    return this.bankAccountsRepo.findMany({
+      where: { userId },
+    });
+  }
+
+  update(id: number, updateBankAccountDto: UpdateBankAccountDto) {
+    return `This action updates a #${id} bankAccount`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} bankAccount`;
+  }
+}
